b37c55b6c10bd14aced2c4b23137e71daa088265
peterhedley <hedleyplh@gmail.com>
2018-02-15 13:50:04 +0000

    Softmax gradient _ version_1

--- src/classifiers.py
+++ src/classifiers.py
@@ -25,7 +25,21 @@ def softmax(logits, y):
     ###########################################################################
     #                           BEGIN OF YOUR CODE                            #
     ###########################################################################
-
+    loss = np.exp(logits-np.max(logits)) / np.sum(np.exp(logits-np.max(logits)), axis=0)
+    SM = logits.reshape((5,1,-1))
+    print(SM.shape)
+    print("****************")
+    
+    print(np.transpose(SM,(0,2,1)).shape)
+    tenbyten = [np.dot(x,y) for y,x in zip(SM,np.transpose(SM,(0,2,1)))]
+    diags = [np.diag(y) for y in logits]
+    print(diags[0].shape)
+    #tenbyten = np.dot(SM,np.transpose(SM,(0,2,1)))
+    next_array = [x - y for x,y in zip(diags,tenbyten)]
+    print(next_array[0].shape)
+    print(logits.shape)
+    dlogits = np.array([np.dot(y,x) for x,y in zip(next_array,logits)])
+    print(dlogits.shape)
 
     ###########################################################################
     #                            END OF YOUR CODE                             #
