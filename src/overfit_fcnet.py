81561ebc74193d93fa7d197fe5a350004a54d10c
Greg Collinge <gc611@edge02.doc.ic.ac.uk>
2018-03-05 17:21:33 +0000

    Put report parameters into overfit and train

--- src/overfit_fcnet.py
+++ src/overfit_fcnet.py
@@ -11,7 +11,7 @@ TODO: Overfit the network with 50 samples of CIFAR-10
 #                           BEGIN OF YOUR CODE                            #
 ###########################################################################
 data = get_CIFAR10_data(num_training = 50)
-model = FullyConnectedNet(hidden_dims=[128], reg=0.0, num_classes=10)
+model = FullyConnectedNet(hidden_dims=[128], reg=0.0, num_classes=10, dtype = np.float64)
 
 solver = Solver(model, data,
                 update_rule='sgd',
@@ -20,6 +20,22 @@ solver = Solver(model, data,
                 batch_size=25,
                 print_every=100)
 solver.train()
+
+
+import matplotlib.pyplot as plt
+plt.subplot(2, 1, 1)
+plt.title("Training loss")
+plt.plot(solver.loss_history, "o")
+plt.xlabel('Iteration')
+plt.subplot(2, 1, 2)
+plt.title('Accuracy')
+plt.plot(solver.train_acc_history, '-o', label='train')
+plt.plot(solver.val_acc_history, '-o', label='val')
+plt.plot([0.5] * len(solver.val_acc_history), 'k--')
+plt.xlabel('Epoch')
+plt.legend(loc='lower right')
+plt.gcf().set_size_inches(15, 12)
+plt.show()
 ##############################################################################
 #                             END OF YOUR CODE                               #
 ##############################################################################
--- src/train_fcnet.py
+++ src/train_fcnet.py
@@ -12,22 +12,23 @@ accuracy on the validation set.
 #                           BEGIN OF YOUR CODE                            #
 ###########################################################################
 data = get_CIFAR10_data()
-model = FullyConnectedNet(hidden_dims=[128], reg=1e-3, num_classes=10)
+model = FullyConnectedNet(hidden_dims=[128], reg=1e-4, num_classes=10, dtype = np.float64)
 
 solver = Solver(model, data,
                 update_rule='sgd',
                 optim_config={'learning_rate': 1e-3,}, lr_decay = 0.85,
-                num_epochs=100, batch_size=70,
-                print_every=100)
+                num_epochs=30, batch_size=65,
+                print_every=1000)
 solver.train()
 
-solver._save_checkpoint() #save
+acc = solver.check_accuracy(data['X_train'], data['y_train'])
+print("Training accuracy {} on {} examples".format(acc, len(data['X_train'])))
+acc = solver.check_accuracy(data['X_val'], data['y_val'])
+print("validation accuracy {} on {} examples".format(acc, len(data['X_val'])))
+acc = solver.check_accuracy(data['X_test'], data['y_test'])
+print("Test accuracy {} on {} examples".format(acc, len(data['X_test'])))
 
-##############################################################################
-#                             END OF YOUR CODE                               #
-##############################################################################
 import matplotlib.pyplot as plt
-
 plt.subplot(2, 1, 1)
 plt.title("Training loss")
 plt.plot(solver.loss_history, "o")
@@ -41,3 +42,6 @@ plt.xlabel('Epoch')
 plt.legend(loc='lower right')
 plt.gcf().set_size_inches(15, 12)
 plt.show()
+##############################################################################
+#                             END OF YOUR CODE                               #
+##############################################################################
