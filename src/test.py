87885a1a0b8464faf084eec0b4ca617282a3e86e
Kiyohito Kunii <kk3317@edge03.doc.ic.ac.uk>
2018-03-05 15:16:12 +0000

    add confusion metrix, recall and precision in test.py

--- src/test.py
+++ src/test.py
@@ -6,6 +6,8 @@ import glob
 from scipy import misc
 from PIL import Image
 
+from eval.matrix import *
+
 # Make this a member function of the model?
 def predict(X,batch_size=100, model = None):
     N = X.shape[0]
@@ -70,8 +72,6 @@ def test_fer_model(img_folder, model_path):
     predictions = np.concatenate(predictions)
     return  predictions
 
-
-
 overall_pred = test_fer_model(path_to_images, path_to_model)
 from src.get_acc import get_labels
 labs = get_labels()
@@ -93,6 +93,28 @@ f1_score = f1_score(labs_in_order, overall_pred, average=None)
 print("F1 Score: ")
 print(f1_score)
 
+recall = recall_score(labs_in_order, overall_pred, average=None)
+print("Recall: ")
+print(recall)
+
+precision = precision_score(labs_in_order, overall_pred, average=None)
+print("Precision: ")
+print(precision)
+
 confusion = confusion_matrix(labs_in_order, overall_pred)
 print("Confusion: ")
 print(confusion)
+
+np.set_printoptions(precision=2)
+# Plot non-normalized confusion matrix
+plt.figure()
+class_names = labels = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
+plot_confusion_matrix(confusion, classes=class_names,
+                      title='Confusion matrix, without normalization')
+
+# Plot normalized confusion matrix
+plt.figure()
+plot_confusion_matrix(confusion, classes=class_names, normalize=True,
+                      title='Normalized confusion matrix')
+
+plt.show()
