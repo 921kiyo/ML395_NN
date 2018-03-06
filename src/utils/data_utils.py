9ead639501f644ff2ce5b19763c0e0adeb61eb44
peterhedley <hedleyplh@gmail.com>
2018-03-06 14:09:54 +0000

    data_utils update

--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -47,9 +47,66 @@ def get_CIFAR10_data(num_training=49000, num_validation=1000, num_test=1000,
     condensed to a single function.
     """
     # Load the raw CIFAR-10 data
-    path = "/vol/bitbucket/395ML_NN_Data/"
+    #path = "/vol/bitbucket/395ML_NN_Data/"
+    path = "C:/Users/Peter/Documents/Machine_Learning/ML395_NN"
     cifar10_dir = os.path.join(path,'datasets','cifar-10-batches-py')
     X_train, y_train, X_test, y_test = load_CIFAR10(cifar10_dir)
+
+    # Subsample the data
+    mask = list(range(num_training, num_training + num_validation))
+    X_val = X_train[mask]
+    y_val = y_train[mask]
+    mask = list(range(num_training))
+    X_train = X_train[mask]
+    y_train = y_train[mask]
+    mask = list(range(num_test))
+    X_test = X_test[mask]
+    y_test = y_test[mask]
+
+    # Normalize the data: subtract the mean image
+    if subtract_mean:
+        mean_image = np.mean(X_train, axis=0)
+        X_train -= mean_image
+        X_val -= mean_image
+        X_test -= mean_image
+
+    # Transpose so that channels come first
+    X_train = X_train.transpose(0, 3, 1, 2).copy()
+    X_val = X_val.transpose(0, 3, 1, 2).copy()
+    X_test = X_test.transpose(0, 3, 1, 2).copy()
+
+    # Package data into a dictionary
+    return {
+      'X_train': X_train, 'y_train': y_train,
+      'X_val': X_val, 'y_val': y_val,
+      'X_test': X_test, 'y_test': y_test,
+    }
+
+def load_FER_2013(filename):
+    """ load single batch of cifar """
+    with open(filename, 'rb') as f:
+        datadict = load_pickle(f)
+        print(datadict)
+        X_train = datadict['X_train']
+        X_test = datadict['X_test']
+        Y_train = datadict['y_train']
+        Y_test = datadict['X_train']
+        return X_train, Y_train, X_test, Y_test
+
+
+def get_FeR2013_data(num_training=27709, num_validation=1000, num_test=1000,
+                     subtract_mean=True):
+    """
+    Load the CIFAR-10 dataset from disk and perform preprocessing to prepare
+    it for classifiers. These are the same steps as we used for the SVM, but
+    condensed to a single function.
+    """
+    # Load the raw CIFAR-10 data
+    #path = "/vol/bitbucket/395ML_NN_Data/"
+    path = "C:/Users/Peter/Documents/Machine_Learning/ML395_NN"
+    fer2013_dir = os.path.join(path,'datasets')#,'Fer2013pu','public')
+    X_train, y_train, X_test, y_test = load_FER_2013(os.path.join(fer2013_dir,"FER2013_data.pickle"))
+
 
     # Subsample the data
     mask = list(range(num_training, num_training + num_validation))
