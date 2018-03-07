108697cb63ac721a7b22968e15b5fb1d1937f714
greg <greg.collinge@outlook.com>
2018-03-07 10:10:07 +0000

    Added JPEG loader and tidied test script

--- src/test.py
+++ src/test.py
@@ -1,31 +1,16 @@
-# Function to load an test the model for Q5
-import numpy as np
+'''''''''
+This file contains the implementations of the prediction functions for Q5 and Q6:
+* Example usage of both prediction functions can be seen in src.generate_result.py
+* Question 6 was implemented using Keras with the Tensorflow back-end
+* The order of predictions is that which is given by sorted(glob.glob(img_folder + "/*.jpg")) on the target directory
+* The model submitted for Q5 is at:
+* The model submitted for Q6 is at:
+'''''''''
 import pickle
 import os
 import glob
-from scipy import misc
 from PIL import Image
-
 from eval.matrix import *
-
-# Prediction for Q5
-def predict(X,batch_size=100, model = None):
-    N = X.shape[0]
-
-    # Compute predictions in batches
-    num_batches = N // batch_size
-    if N % batch_size != 0:
-        num_batches += 1
-    y_pred = []
-    for i in range(num_batches):
-        start = i * batch_size
-        end = (i + 1) * batch_size
-        scores = model.loss(X[start:end])
-        y_pred.append(np.argmax(scores, axis=1))
-    y_pred = np.hstack(y_pred)
-
-    return y_pred
-
 
 def test_fer_model(img_folder="/vol/bitbucket/395ML_NN_Data/datasets/FER2013/Test", model_path="/homes/kk3317/Desktop/ML395_NN/pkl/Q5mod_epoch_20.pkl"):
     # Load the model from pickle and set to testing mode
@@ -44,11 +29,12 @@ def test_fer_model(img_folder="/vol/bitbucket/395ML_NN_Data/datasets/FER2013/Tes
     test_data = []
     n_batch = 0
     for i in range(0,n):
-        # Load a batch of grayscale images
+        # Load a batch of grayscale images to predict on
         im = Image.open(os.path.join(img_folder,image_names[i])) #im = misc.imread(os.path.join(img_folder,image_names[i]), mode= 'F')
         im = im.convert('F')
+        # Subtract the mean of training set the model was trained with from each image
+        im -= np.squeeze(model.mean_image, axis=2)
 
-        # SUBTRACT mean here
         imex = np.expand_dims(im, axis=0)
         test_data.append(imex)
         n_batch += 1
@@ -64,28 +50,30 @@ def test_fer_model(img_folder="/vol/bitbucket/395ML_NN_Data/datasets/FER2013/Tes
     predictions = np.concatenate(predictions)
     return  predictions
 
-#from new_vgg import *
-from common import *
-from vgg_net import *
-import glob
-from PIL import Image
-import keras
 
-
-# Prediction for Q5
-def predict_deep(X,batch_size=100, model = None):
+# Prediction function for Q5 feedforward network
+def predict(X,batch_size=100, model = None):
     N = X.shape[0]
-    X = np.expand_dims(X,axis=3)
-    X = X/255
-    vgg = VGG(cached_model=model)
-    predictions = np.argmax(vgg.model.predict(X,batch_size=100), axis=1)
-    return predictions
+
+    # Compute predictions in batches
+    num_batches = N // batch_size
+    if N % batch_size != 0:
+        num_batches += 1
+    y_pred = []
+    for i in range(num_batches):
+        start = i * batch_size
+        end = (i + 1) * batch_size
+        scores = model.loss(X[start:end])
+        y_pred.append(np.argmax(scores, axis=1))
+    y_pred = np.hstack(y_pred)
+
+    return y_pred
 
 def test_deep_fer_model(img_folder="/vol/bitbucket/395ML_NN_Data/datasets/FER2013/Test", model_path='/homes/kk3317/Desktop/ML395_NN/'):
-    model_path = os.path.join(model_path,"src/question6/models/vgg_netvgg.hdf5")
     # Get image names
     image_names = sorted(glob.glob(img_folder + "/*.jpg"))
     n = len(image_names)
+
     # Load images and predict in batches
     batch_size = 1000
 
@@ -94,15 +82,12 @@ def test_deep_fer_model(img_folder="/vol/bitbucket/395ML_NN_Data/datasets/FER201
     n_batch = 0
     for i in range(0,n):
         # Load a batch of grayscale images
-        im = Image.open(image_names[i]) #im = misc.imread(os.path.join(path_to_images,image_names[i]), mode= 'F')
+        im = Image.open(image_names[i])
         im = im.convert('F')
-
-        # SUBTRACT mean here
         imex = np.expand_dims(im, axis=0)
-        #imex = np.expand_dims(im, axis=4)
-
         test_data.append(imex)
         n_batch += 1
+
         if n_batch == batch_size or i == n-1:
             # Predict on the batch and append results to overall predictions
             con = np.concatenate(test_data, axis=0)
@@ -116,6 +101,13 @@ def test_deep_fer_model(img_folder="/vol/bitbucket/395ML_NN_Data/datasets/FER201
     print("predictions ", predictions)
     return  predictions
 
-# TODO DElete this
-model_path = '/homes/kk3317/Desktop/ML395_NN/'
-overall_pred = test_deep_fer_model(model_path = model_path)
+# Prediction for Q6 CNN
+def predict_deep(X,batch_size=100, model = None):
+    from vgg_net import VGG #Only import tensorflow when using the deep network
+    N = X.shape[0]
+    X = np.expand_dims(X,axis=3)
+    # Apply normalisation of test image to match training range of 0-1
+    X = X/255
+    vgg = VGG(cached_model=model)
+    predictions = np.argmax(vgg.model.predict(X,batch_size=100), axis=1)
+    return predictions
--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -2,8 +2,9 @@ from builtins import range
 from six.moves import cPickle as pickle
 import numpy as np
 import os
-from scipy.misc import imread
 import platform
+from PIL import Image
+import glob
 
 def load_pickle(f):
     version = platform.python_version_tuple()
@@ -79,34 +80,57 @@ def get_CIFAR10_data(num_training=49000, num_validation=1000, num_test=1000,
     return {
       'X_train': X_train, 'y_train': y_train,
       'X_val': X_val, 'y_val': y_val,
-      'X_test': X_test, 'y_test': y_test,
-    }
+      'X_test': X_test, 'y_test': y_test}
 
 def load_FER_2013(filename):
     """ load single batch of cifar """
     with open(filename, 'rb') as f:
         datadict = load_pickle(f)
-        print(datadict)
         X_train = datadict['X_train']
         X_test = datadict['X_test']
         Y_train = datadict['y_train']
-        Y_test = datadict['X_train']
+        Y_test = datadict['y_test']
         return X_train, Y_train, X_test, Y_test
 
 
-def get_FeR2013_data(num_training=27709, num_validation=1000, num_test=1000,
-                     subtract_mean=True):
+# JPEG version of image loader
+def load_FER_2013_jpg(fer_folder, subtract_mean = True,num_training=27709, num_validation=1000, num_test=1000):
     """
-    Load the CIFAR-10 dataset from disk and perform preprocessing to prepare
-    it for classifiers. These are the same steps as we used for the SVM, but
-    condensed to a single function.
+    Load the FER2013 dataset from disk and perform preprocessing to prepare
+    it for classifiers. The Pickle version
     """
-    # Load the raw CIFAR-10 data
-    #path = "/vol/bitbucket/395ML_NN_Data/"
-    path = "C:/Users/Peter/Documents/Machine_Learning/ML395_NN"
-    fer2013_dir = os.path.join(path,'datasets')
-    X_train, y_train, X_test, y_test = load_FER_2013(os.path.join(fer2013_dir,"FER2013_data.pickle"))
+    # Get labels ordered in the same way as the images
+    y_train = labels_in_order(fer_folder, set = 'train')
+    y_test = labels_in_order(fer_folder, set = 'test')
 
+    # Get the images from disk
+    train_path = os.path.join(fer_folder, "Train")
+    test_path = os.path.join(fer_folder, "Test")
+
+    image_names = sorted(glob.glob(test_path + "/*.jpg"))
+    n = len(image_names)
+    test_data = []
+    for i in range(0, n):
+        im = Image.open(image_names[i])
+        im = im.convert('F')
+        imex = np.expand_dims(im, axis=0)
+        test_data.append(imex)
+
+    X_test = np.concatenate(test_data, axis=0)
+    X_test = np.expand_dims(X_test, axis=3)
+
+    image_names = sorted(glob.glob(train_path + "/*.jpg"))
+
+    n = len(image_names)
+    train_data = []
+    for i in range(0, n):
+        im = Image.open(image_names[i])
+        im = im.convert('F')
+        imex = np.expand_dims(im, axis=0)
+        train_data.append(imex)
+
+    X_train = np.concatenate(train_data, axis=0)
+    X_train = np.expand_dims(X_train, axis=3)
 
     # Subsample the data
     mask = list(range(num_training, num_training + num_validation))
@@ -133,7 +157,85 @@ def get_FeR2013_data(num_training=27709, num_validation=1000, num_test=1000,
 
     # Package data into a dictionary
     return {
-      'X_train': X_train, 'y_train': y_train,
-      'X_val': X_val, 'y_val': y_val,
-      'X_test': X_test, 'y_test': y_test,'mean_image':mean_image
-    }
+        'X_train': X_train, 'y_train': y_train,
+        'X_val': X_val, 'y_val': y_val,
+        'X_test': X_test, 'y_test': y_test,
+        'mean_image' : mean_image}
+
+
+
+
+# Pickle version
+def get_FeR2013_data(num_training=27709, num_validation=1000, num_test=1,
+                     subtract_mean=True):
+    """
+    Load the FER2013 dataset from a Pickle on disk and perform preprocessing to prepare
+    it for classifiers. The Pickle version
+    """
+    # Load pickle
+    #path = "/vol/bitbucket/395ML_NN_Data/"
+    #path = "C:/Users/Peter/Documents/Machine_Learning/ML395_NN"
+    path = "/home/greg/Desktop/Q5_Final/ML395_NN"
+    fer2013_dir = os.path.join(path,'datasets')
+    X_train, y_train, X_test, y_test = load_FER_2013(os.path.join(fer2013_dir,"FER2013_data.pickle"))
+
+    # Subsample the data
+    mask = list(range(num_training, num_training + num_validation))
+    X_val = X_train[mask]
+    y_val = y_train[mask]
+    mask = list(range(num_training))
+    X_train = X_train[mask]
+    y_train = y_train[mask]
+    mask = list(range(num_test))
+    X_test = X_test[mask]
+    y_test = y_test[mask]
+
+    # Normalize the data: subtract the mean image
+    if subtract_mean:
+        mean_image = np.mean(X_train, axis=0)
+        X_train -= mean_image
+        X_val -= mean_image
+        X_test -= mean_image
+
+    # Transpose so that channels come first
+    X_train = X_train.transpose(0, 3, 1, 2).copy()
+    X_val = X_val.transpose(0, 3, 1, 2).copy()
+    X_test = X_test.transpose(0, 3, 1, 2).copy()
+
+
+    # Package data into a dictionary
+    return {
+        'X_train': X_train, 'y_train': y_train,
+        'X_val': X_val, 'y_val': y_val,
+        'X_test': X_test, 'y_test': y_test,'mean_image':mean_image}
+
+# Helper function to create a dictionary of labels
+def labels_dictionary(fer_folder):
+    path_to_labels = os.path.join(fer_folder, 'labels_public.txt')
+    labs = open(path_to_labels).readlines()
+
+    labels = {}
+    train = {}
+    test = {}
+    for j in range(1,len(labs)):
+        identifier = labs[j].partition('/')[0]
+        parts = labs[j].partition(',')
+        name = parts[0]
+        num = int(parts[2][0])
+        if identifier == 'Train':
+            train[name] = num
+        if identifier == 'Test':
+            test[name] = num
+
+    labels['train'] = train
+    labels['test'] = test
+    return labels
+
+# Helper function to order labels in the same order as the images
+def labels_in_order(fer_folder, set = "test"):
+
+    labels_dict = labels_dictionary(fer_folder)
+    y_keys = sorted(list(labels_dict[set].keys()))
+    labels = np.array([labels_dict[set][x] for x in y_keys])
+
+    return labels
